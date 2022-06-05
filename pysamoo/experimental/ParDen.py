2642397002f6d1c82801facbb7e0eccbbb1bfb32
tvanzyl <tvanzyl@gmail.com>
2022-06-05 16:44:16 +0200

    ParDen 2.0 Done

--- pysamoo/experimental/ParDen.py
+++ pysamoo/experimental/ParDen.py
@@ -3,21 +3,9 @@ from copy import deepcopy
 
 import matplotlib.pyplot as plt
 import numpy as np
-from pymoo.algorithms.moo.nsga2 import RankAndCrowdingSurvival
-from pymoo.algorithms.soo.nonconvex.ga import FitnessSurvival
-from pymoo.core.duplicate import DefaultDuplicateElimination
-from pymoo.core.evaluator import Evaluator
 from pymoo.core.population import Population
-from pymoo.operators.sampling.lhs import LHS
-from pymoo.operators.selection.tournament import compare
-from pymoo.optimize import default_termination
-from pymoo.util.display import Display
-from pymoo.util.dominator import get_relation
-from pymoo.util.misc import norm_eucl_dist, cdist
 from pymoo.util.optimum import filter_optimum
 from pymoo.util.termination.no_termination import NoTermination
-from pymoo.visualization.fitness_landscape import FitnessLandscape
-from pymoo.visualization.video.callback_video import AnimationCallback
 
 from pysamoo.core.algorithm import SurrogateAssistedAlgorithm
 # =========================================================================================================
